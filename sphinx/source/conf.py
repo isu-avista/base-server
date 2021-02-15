38ea25af587d950a197fdf6c1a2f9af422b32b51
Isaac Griffith <grifisaa@isu.edu>
2021-02-14 22:04:10 -0700

    updated documentation. ready for release

--- sphinx/source/conf.py
+++ sphinx/source/conf.py
@@ -23,7 +23,7 @@ copyright = '2020-2021, Isaac Griffith'
 author = 'Isaac Griffith'
 
 # The full version, including alpha/beta/rc tags
-release = 'v0.2.0'
+release = 'v0.3.1'
 
 
 # -- General configuration ---------------------------------------------------
